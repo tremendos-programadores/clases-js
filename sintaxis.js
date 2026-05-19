da26fd00984ec256cdf1066d18ff8b417bed8116
UrielAraujoGit <urielaraujo@live.com.ar>
2026-05-18 21:40:18 -0300

    function syntax

--- sintaxis.js
+++ sintaxis.js
@@ -1,3 +1,5 @@
+// VARIABLES Y CONSTANTES
+
 // programación tenemos 2 formas de almacenar datos
 // 1 - variables: contenedor nombrado donde se almacenan datos que pueden variar con el tiempo
 // 2 - constantes: contendor nombrado donde se almacenan datos que NO pueden variar con el tiempo
@@ -15,41 +17,42 @@
 // edad = 1;
 // console.log(edad)
 
-const zara = {
-    lujo: true,
-    caro: true,
-    para_morochos: false,
-    locales: 8201,
-    dobla: "derecha"
-}
+// CONDICIONALES
 
-const ojota = {
-    talle: 42, color: "negro", rotas: false,
-    marca: zara,
-    material: {
-        goma: 60,
-        pintura: 3,
-        otros: {
-            reciclabes: 12,
-            no_reciclables: 25
-        }
+// que son los condicionales en js ?
+// son "estructuras de control" que nos permiten tomar desiciones en el código,
+// con una lógica definida por nosotros
+
+// ejemplo:
+// una condición puede ser que solo las personas con al menos 18 años puedan comprar alcohol
+
+// if es una palabra reservada para definir una condición
+
+//if (edad >= 18) {
+//    // ...
+//}
+
+
+//const edad_viejo = 90;
+//if (edad_viejo >= 18) {
+//    console.log("puede comprar alcohol")
+//}
+
+// const edad_pendejo = 16;
+// if (edad_pendejo >= 18) {
+//    console.log("puede comprar alcohol")
+// }
+
+function puedeComprarAlcohol(edad) {
+    const puede = edad >= 18;
+    if (puede) {
+        console.log("puede comprar alcohol")
     }
+    return puede
 }
 
-zara.clasista = true;
 
-
-const ojota2 = ojota;
-ojota2.color = "blanco";
-
-let nombre1 = "pepe";
-let nombre2=nombre1;
-nombre2= "juanito" 
-
-// ojota = {} // MAL!!
-// ojota.talle = 43 // válido
-
-//ojota.material.goma=8000
-
-//console.log("tiene " + JSON.stringify(ojota) + "% de goma")
-console.log(JSON.stringify(ojota))
+const viejo_puede_comprar_chupi = puedeComprarAlcohol(90);
+const pibe_puede_comoprar_chupi = puedeComprarAlcohol(16);
+console.log(viejo_puede_comprar_chupi);
+console.log(pibe_puede_comoprar_chupi)
